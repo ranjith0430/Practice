5f30c149edd0b795fff23bc4d737d647a303744f
Ranjith Kumar Mittapelli <35166565+ranjith0430@users.noreply.github.com>
2022-08-18 15:33:17 +0530

    Create CircularArrayRotation.java
    CirculatArrayRotation-Hackerrank

---
+++ CircularArrayRotation.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+  
+  /*
+  John Watson knows of an operation called a right circular rotation on an array of integers. One rotation operation moves the last array element to the first
+  position and shifts all remaining elements right one. To test Sherlock's abilities, Watson provides Sherlock with an array of integers. Sherlock is to perform the
+  rotation operation a number of times then determine the value of the element at a given position. For each array, perform a number of right circular rotations and
+  return the values of the elements at the given indices.
+  */
+
+    /*
+     * Complete the 'circularArrayRotation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY queries
+     */
+
+    public static List<Integer> circularArrayRotation(List<Integer> a, int k, List<Integer> queries) {
+    // Write your code here
+    while(k>0){
+        a.add(0,a.get(a.size()-1));//add last element to first
+        a.remove(a.size()-1);//remove last element
+        k--;
+    }
+    List<Integer> output=new ArrayList<>();
+    for(int i=0;i<queries.size();i++){
+        output.add(a.get(queries.get(i)));
+    }
+    return output;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int q = Integer.parseInt(firstMultipleInput[2]);
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> queries = IntStream.range(0, q).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.circularArrayRotation(a, k, queries);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
